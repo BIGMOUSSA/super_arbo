022e10ddd65fc6d7b0859117d13fa5fbee9a0c2d
Moussa DIALLO <diallomous@gmail.com>
2023-09-19 18:48:23 +0000

    method 1 with argparse

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from utils import git_repo_init, create_project_data_path
 import os
+import argparse
+
+
+parser = argparse.ArgumentParser(description = "Application for folder and files creator for you data works")
+
+parser.add_argument('path', help = 'the path to the folder')
+
+args = parser.parse_args()
 
 class SuperFolder :
     
@@ -13,10 +21,8 @@ class SuperFolder :
         git_repo_init(self.mypath)
 
 if  __name__ == '__main__':
-    #os.mkdir("testFolderPath3")
-
-    testPath = "test_m1"
-    os.makedirs(testPath, exist_ok=True)
-    DataCreator1 = SuperFolder(testPath)
+    given_Path = args.path
+    os.makedirs(given_Path, exist_ok=True)
+    DataCreator1 = SuperFolder(given_Path)
     DataCreator1.PathCreator()
     DataCreator1.Init()
